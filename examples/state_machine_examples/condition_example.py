6a15a6381213c294e5807b4af07ed1b353081694
Carlos Mão de Ferro <carlos@macminicmf.neuro>
2017-01-30 12:46:15 +0000

    condition_example adapted

--- examples/state_machine_examples/condition_example.py
+++ examples/state_machine_examples/condition_example.py
@@ -1,48 +1,67 @@
-'''
-----------------------------------------------------------------------------
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
 
-This file is part of the Sanworks Bpod repository
-Copyright (C) 2016 Sanworks LLC, Sound Beach, New York, USA
+"""
+Example adapted from Josh Sanders' original version on Sanworks Bpod repository
+"""
 
-----------------------------------------------------------------------------
+import logging
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, version 3.
+import examples.settings as settings
 
-This program is distributed  WITHOUT ANY WARRANTY and without even the
-implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-See the GNU General Public License for more details.
+from pybpodapi.model.bpod import Bpod
+from pybpodapi.model.state_machine import StateMachine
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-'''
+logger = logging.getLogger("examples")
 
-import os, sys
-sys.path.append(os.path.join(os.path.dirname(__file__)[:-22], "Modules")) # Add Bpod system files to Python path
 
-from BpodClass import BpodObject # Import BpodObject
-from StateMachineAssembler import stateMachine # Import state machine assembler
+def run():
+	"""
+	Run this protocol now
+	"""
 
-myBpod = BpodObject('COM13') # Create a new instance of a Bpod object on serial port COM13
+	my_bpod = Bpod().start(settings.SERIAL_PORT)
 
-sma = stateMachine(myBpod) # Create a new state machine (events + outputs tailored for myBpod)
-sma.setCondition(1, 'Port2', 1) #Arguments: (ConditionNumber, ConditionChannel, ConditionValue; 1 = high/in, 0 = low/out)
-sma.addState('Name', 'Port1Light', # Add a state
-             'Timer', 1,
-             'StateChangeConditions', ('Tup', 'Port2Light'),
-             'OutputActions', ('PWM1', 255))
-sma.addState('Name', 'Port2Light',
-             'Timer', 1,
-             'StateChangeConditions', ('Tup', 'Port3Light', 'Condition1', 'Port3Light'),
-             'OutputActions', ('PWM2', 255))
-sma.addState('Name', 'Port3Light',
-             'Timer', 1,
-             'StateChangeConditions', ('Tup', 'exit'),
-             'OutputActions', ('PWM3', 255))
-print sma.conditions.matrix
-myBpod.sendStateMachine(sma) # Send state machine description to Bpod device
-RawEvents = myBpod.runStateMachine() # Run state machine and return events
-print RawEvents.__dict__ # Print events to console
+	sma = StateMachine(my_bpod.hardware)
 
-myBpod.disconnect() # Disconnect Bpod
+	sma.set_condition(condition_number=1, condition_channel='Port2', channel_value=1)
+
+	sma.add_state(
+		state_name='Port1Light',
+		state_timer=1,
+		state_change_conditions={'Tup': 'Port2Light'},
+		output_actions=[('PWM1', 255)])
+
+	sma.add_state(
+		state_name='Port2Light',
+		state_timer=1,
+		state_change_conditions={'Tup': 'Port3Light', 'Condition1': 'Port3Light'},
+		output_actions=[('PWM2', 255)])
+
+	sma.add_state(
+		state_name='Port3Light',
+		state_timer=1,
+		state_change_conditions={'Tup': 'exit'},
+		output_actions=[('PWM3', 255)])
+
+	logger.info("Conditions matrix: %s", sma.conditions.matrix)
+
+	my_bpod.send_state_machine(sma)  # Send state machine description to Bpod device
+
+	raw_events = my_bpod.run_state_machine(sma)  # Run state machine and return events
+
+	logger.info("Raw events: %s", raw_events)
+
+	my_bpod.disconnect()  # Disconnect Bpod
+
+
+if __name__ == '__main__':
+	import loggingbootstrap
+
+	# setup different loggers for example script and api
+	loggingbootstrap.create_double_logger("pybpodapi", settings.API_LOG_LEVEL, 'pybpodapi-examples.log',
+	                                      settings.API_LOG_LEVEL)
+	loggingbootstrap.create_double_logger("examples", settings.EXAMPLE_SCRIPT_LOG_LEVEL, 'pybpodapi-examples.log',
+	                                      settings.EXAMPLE_SCRIPT_LOG_LEVEL)
+
+	run()
