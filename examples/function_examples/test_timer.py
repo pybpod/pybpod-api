791e45177179d835fb822237748717761df77d05
Carlos Mão de Ferro <cajomferro@gmail.com>
2017-06-09 17:56:10 +0100

    Example for testing timers

---
+++ examples/function_examples/test_timer.py
@@ -0,0 +1,97 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+Testing timer intervals between trials
+
+@authors: Rachid Azizi, Carlos Mão de Ferro, Ricardo Ribeiro
+"""
+
+import random
+
+import examples.settings as settings
+
+from pybpodapi.model.bpod import Bpod
+from pybpodapi.model.state_machine import StateMachine
+from pybpodapi.hardware.events import EventName
+from pybpodapi.hardware.output_channels import OutputChannel
+from functools import reduce
+
+import time
+
+my_bpod = Bpod().start(settings.SERIAL_PORT, settings.WORKSPACE_PATH, "test_timer")
+
+
+def run():
+	nTrials = 30
+
+	global_timer = time.time()
+	time1 = time.time()
+	TS = 0.1  # time in each state
+
+	timestamps = []
+
+	for i in range(nTrials):
+		print('Trial: {0}'.format(i + 1))
+
+		sma = StateMachine(my_bpod.hardware)
+
+		sma.add_state(
+			state_name='State1',
+			state_timer=TS,
+			state_change_conditions={EventName.Tup: 'State2'},
+			output_actions=[(OutputChannel.PWM2, 255)])
+		sma.add_state(
+			state_name='State2',
+			state_timer=TS,
+			state_change_conditions={EventName.Tup: 'State3'},
+			output_actions=[(OutputChannel.PWM2, 255)])
+		sma.add_state(
+			state_name='State3',
+			state_timer=TS,
+			state_change_conditions={EventName.Tup: 'State4'},
+			output_actions=[(OutputChannel.Valve, 1)])
+		sma.add_state(
+			state_name='State4',
+			state_timer=TS,
+			state_change_conditions={EventName.Tup: 'State5'},
+			output_actions=[(OutputChannel.PWM2, 255)])
+		sma.add_state(
+			state_name='State5',
+			state_timer=TS,
+			state_change_conditions={EventName.Tup: 'exit'},
+			output_actions=[(OutputChannel.Valve, 1)])
+
+		# time.sleep(0.1)
+
+		#print("\nTime before send: {0}".format(time.time() - time1))
+		my_bpod.send_state_machine(sma)  # Send state machine description to Bpod device
+		#print("\nTime after send: {0}".format(time.time() - time1))
+
+		#print("\nTime before run: {0}".format(time.time() - time1))
+		my_bpod.run_state_machine(sma)  # Run state machine
+		#print("\nTime after run: {0}".format(time.time() - time1))
+
+
+		timestamps.append(time.time() - time1)
+		#print("\nTotal trial time: {0}".format(time.time() - time1))
+
+		#print("\nGlobal timer: {0}".format(time.time() - global_timer))
+
+		time1 = time.time()
+
+	print("Trial lenght mean: {0}".format(reduce(lambda x, y: x + y, timestamps) / len(timestamps)))
+
+
+if __name__ == '__main__':
+	settings.run_this_protocol(run)
+	# BPOD_INSTANCE = Bpod().start(settings.SERIAL_PORT, settings.WORKSPACE_PATH, "test_timer")
+	#
+	# protocol_path = "/Users/carlos/Dropbox/Projects/champalimaud-projects/pycontrol/pycontrol-examples/test_bpod/tasks/test_timer.py.py"
+	#
+	# print(protocol_path)
+	#
+	# ldict = locals()
+	# exec(open(protocol_path).read(), globals(), ldict)
+	# mybpod = ldict['my_bpod']
+	# BPOD_INSTANCE.stop()
